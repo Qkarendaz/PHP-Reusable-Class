fb3925f9e6392ca417917df6eae2d006c75ae442
Stephen Peters <36298988+Qkarendaz@users.noreply.github.com>
2020-11-25 16:37:56 +0100

    Update dbh.php

--- classes/dbh.php
+++ classes/dbh.php
@@ -1,9 +1,9 @@
 <?php
 
 class Dbh {
-    private $hostname = "localhost";
-    private $username = "root";
-    private $password = "";
+    private $hostname = "localhost"; //Host addess goes here
+    private $username = "root"; //Host address username goes here
+    private $password = ""; //Host adress password goes here
     private $dbname = "newdb"; //database name goes here
 
     protected function conn(){
